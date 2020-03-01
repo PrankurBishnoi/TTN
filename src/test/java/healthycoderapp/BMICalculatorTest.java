3258ce3767d2b8fcd5f78bd177bfc7e9bcc078c2
ShivamAgarwal <shivamagarwal531998@gmail.com>
2020-03-01 22:19:53 +0530

    commiting

--- src/test/java/healthycoderapp/BMICalculatorTest.java
+++ src/test/java/healthycoderapp/BMICalculatorTest.java
@@ -1,14 +1,18 @@
 package healthycoderapp;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BMICalculatorTest {
 
     @Test
-    void fixMe(){
-        assertTrue(true);
+    void shouldThrowException(){
+
+        Executable exec = ()->BMICalculator.isDietRecommended(70,0);
+        assertThrows(ArithmeticException.class,exec);
     }
 
 }
